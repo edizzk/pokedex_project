1d154b097a67e18802dd7bb25e238f19bfc10eaa
edizzk <edizmetekurt@gmail.com>
2022-06-15 01:12:11 +0300

    use dio to get api

--- lib/services/pokdex_api.dart
+++ lib/services/pokdex_api.dart
@@ -1,5 +1,6 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
 import '../model/pokemon_model.dart';
 
 class PokeApi {
@@ -10,8 +11,13 @@ class PokeApi {
     List<PokemonModel> _list = [];
 
     var result = await Dio().get(_url);
+    var pokeList = jsonDecode(result.data)['pokemon'];
 
-    debugPrint(result.toString());
+    if (pokeList is List) {
+      _list = pokeList.map((e) => PokemonModel.fromJson(e)).toList();
+    } else {
+      return [];
+    }
 
     return _list;
   }
